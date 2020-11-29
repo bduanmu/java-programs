d0d26924254bd17378242ce4b6a374c24a0a84bb
bduanmu <74875982+bduanmu@users.noreply.github.com>
2020-11-29 16:04:35 -0500

    Update SongLibrary.java

--- SongLibrary.java
+++ SongLibrary.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Comparator;
 
 class Time {
     int minutes;
@@ -7,6 +9,11 @@ class Time {
     Time(String time) {
         minutes = Integer.parseInt (time.substring(0, 2)); 
         seconds = Integer.parseInt (time.substring(3, 5));
+    }
+    
+    Time(Time time) {
+        minutes = time.minutes; 
+        seconds = time.seconds; 
     }
     
     void add(Time time) {
@@ -24,7 +31,7 @@ class Time {
     }
     
     public String toString() {
-        return minutes + ":" + seconds; 
+        return String.format("%02d:%02d", minutes, seconds);
     }
 }
 
@@ -39,18 +46,26 @@ class Song {
         this.duration = new Time(duration); 
     }
     
-    void print() {
-        System.out.println(title + "\t" + artist + "\t" + duration);
+    public String toString() {
+        return String.format("%-7s", title) + "\t" + artist + "\t" + duration;
     }
 }
 
 class Playlist {
+    String name; 
     ArrayList <Song> songs; 
     Time duration; 
     
-    Playlist() {
+    Playlist(String name) {
+        this.name = name;
         songs = new ArrayList <Song>();
         duration = new Time("00:00");
+    }
+    
+    Playlist(Playlist playlist) {
+        name = playlist.name + "-Copy"; 
+        songs = new ArrayList <Song> (playlist.songs); 
+        duration = new Time(playlist.duration); 
     }
     
     void addSong(Song song) {
@@ -63,22 +78,110 @@ class Playlist {
         songs.remove(index); 
     }
     
+    String getName() {
+        return name.toUpperCase(); 
+    }
+    
     void print() {
+        System.out.println(name + "\t" + duration);
+        int i = 1;
+        System.out.println("Song Title\tArtist\tDuration");
         for (Song song: songs) {
-            song.print(); 
+            System.out.println(i + ". " + song); 
+            i++;
         }
     }
 }
 
 class Library {
+    ArrayList <Playlist> playlists; 
     
+    Library() {
+        playlists = new ArrayList <Playlist>(); 
+    }
+    
+    void createNewPlaylist(String name) {
+        Playlist playlist = new Playlist(name); 
+        playlists.add(playlist); 
+    }
+    
+    void listAllPlaylists() {
+        int i = 1; 
+        if (playlists.size() == 0) {
+            System.out.println("Library is empty. "); 
+        } 
+        for (Playlist playlist: playlists) {
+            System.out.println(i + ". " + String.format("%-10s", playlist.name) + "\t" + playlist.duration);
+            i++; 
+        }
+    }
+    
+    void deletePlaylist(int index) {
+        playlists.remove(index);
+    }
+    
+    void copyPlaylist(int index) {
+        Playlist playlistCopy = new Playlist(playlists.get(index));
+        playlists.add(playlistCopy); 
+    }
+    
+    void sortByName() {
+        playlists.sort(Comparator.comparing(Playlist::getName));
+    }
+    
+    void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            listAllPlaylists();
+            System.out.println("Press \"a\" to create new playlist, \"d\" to delete a playlist, \"c\" to copy a playlist and \"s\" to sort the playlists. Type the index of a playlist to open it. To exit the program, press \"q\"");
+            String input = scanner.nextLine(); 
+            if (input.equals("q")) {
+                break;
+            } else if (input.equals("a")) {
+                System.out.print("Input the name of your playlist: ");
+                input = scanner.nextLine(); 
+                createNewPlaylist(input); 
+            } else if (input.equals("d")) {
+                System.out.print("Input the index of the playlist you want to delete: ");
+                input = scanner.nextLine();
+                deletePlaylist(Integer.parseInt(input) - 1); 
+            } else if (input.equals("c")) {
+                System.out.print("Input the index of the playlist you want to copy: ");
+                input = scanner.nextLine();
+                copyPlaylist(Integer.parseInt(input) - 1); 
+            } else if (input.equals("s")) {
+                sortByName();
+            } else {
+                Playlist playlist = playlists.get(Integer.parseInt(input) - 1);
+                while (true) {
+                    playlist.print(); 
+                    System.out.println("Press \"a\" to add a song and \"d\" to remove a song. Press \"q\" to go back to the library. ");
+                    String songInput = scanner.nextLine();
+                    if (songInput.equals("a")) {
+                        System.out.print("Input the name of your song: ");
+                        String songName = scanner.nextLine(); 
+                        System.out.print("Input the artist of your song: ");
+                        String songArtist = scanner.nextLine(); 
+                        System.out.print("Input the duration of your song in this format: \"MM:SS\": ");
+                        String songDuration = scanner.nextLine(); 
+                        playlist.addSong(new Song(songName, songArtist, songDuration));
+                    } else if (songInput.equals("d")) {
+                        System.out.print("Input the index of the song you want to delete: ");
+                        songInput = scanner.nextLine();
+                        playlist.removeSong(Integer.parseInt(songInput) - 1);
+                    } else if (songInput.equals("q")) {
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
 
 public class SongLibrary
 {
 	public static void main(String[] args) {
-		Playlist playlist = new Playlist(); 
-		playlist.addSong(new Song("title", "artist", "03:12"));
-		playlist.print(); 
+		Library library = new Library(); 
+		library.start(); 
 	}
 }
